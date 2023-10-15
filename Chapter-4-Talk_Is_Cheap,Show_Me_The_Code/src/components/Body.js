0d0ea1db5d1a1920df016fea97c4cf6c9ea5b471
Yash Paota <yashpaota@gmail.com>
2023-10-15 17:46:51 +0530

    Adding API Calling

--- Chapter-4-Talk_Is_Cheap,Show_Me_The_Code/src/components/Body.js
+++ Chapter-4-Talk_Is_Cheap,Show_Me_The_Code/src/components/Body.js
@@ -1,13 +1,31 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
-import resList from "../utils/mockData";
+import { useState , useEffect} from "react";
+import Shimmer from "./Shimmer";
+// import resList from "../utils/mockData";
 
 
 const Body = () => {
-const[listOfRestaurants , setListOfRestaurants] = useState(resList); 
-console.log(listOfRestaurants)  // Super Powerful 
+const[listOfRestaurants , setListOfRestaurants] = useState([]);
+useEffect(() => {
+      fetchData();
+          } , []);
 
-    return(
+const fetchData = async() => {
+     const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+     const json = await data.json();
+     console.log(json);
+     //optional chaining
+     setListOfRestaurants(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+}          
+   //Conditional rendering
+   // if(listOfRestaurants.length === 0)
+   // {
+      
+   //    return <Shimmer />
+   // }
+
+   //using ternary operator
+   return listOfRestaurants.length === 0 ? <Shimmer /> : (
         <div className="body">
            <div className="filter">
               <button 
