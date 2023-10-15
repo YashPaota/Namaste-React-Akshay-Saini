f09d276195ac8ad1aa42349e684d2aa30f572c6c
Yash Paota <yashpaota@gmail.com>
2023-10-15 21:54:54 +0530

    Added useState concept for Login

--- Chapter-4-Talk_Is_Cheap,Show_Me_The_Code/src/components/Header.js
+++ Chapter-4-Talk_Is_Cheap,Show_Me_The_Code/src/components/Header.js
@@ -1,6 +1,8 @@
 import { LOGO_URL } from "../utils/constants";
-
+import { useState } from "react";
 const Header = () => {
+
+    const[btnNameReact , setBtnNameReact] = useState("Login")
     return(
         <div className="header">
             <div className="logo-container">
@@ -12,6 +14,14 @@ const Header = () => {
                     <li>About Us</li>
                     <li>Contact</li>
                     <li>Cart</li>
+                    <button className="login" onClick={() => 
+                       {
+                          btnNameReact === "Login" 
+                          ? setBtnNameReact("Logout")
+                          : setBtnNameReact("Login")
+                        }}>
+                    {btnNameReact}
+                    </button>
                 </ul>
             </div>
         </div>
