4876d8f4f1cba7be011821a303ecfb3f507ecbe0
Yash Paota <yashpaota@gmail.com>
2023-10-17 12:44:39 +0530

    Added MENU_API link

--- Chapter-4-Talk_Is_Cheap,Show_Me_The_Code/src/utils/constants.js
+++ Chapter-4-Talk_Is_Cheap,Show_Me_The_Code/src/utils/constants.js
@@ -3,3 +3,4 @@ export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_l
 
 export const LOGO_URL = "https://img.freepik.com/free-vector/food-shopping-logo-template-design_460848-10299.jpg?w=740&t=st=1696847047~exp=1696847647~hmac=e7831d60ca773da28555b80df90902616102882797c4b412d839daa8ef545535";
 
+export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
