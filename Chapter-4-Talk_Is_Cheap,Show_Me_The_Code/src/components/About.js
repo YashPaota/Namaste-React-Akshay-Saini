fd795c25fd5b71080a9ea3272c245d97a8ab531e
Yash Paota <yashpaota@gmail.com>
2023-10-21 18:40:53 +0530

    Added user in About.js

--- Chapter-4-Talk_Is_Cheap,Show_Me_The_Code/src/components/About.js
+++ Chapter-4-Talk_Is_Cheap,Show_Me_The_Code/src/components/About.js
@@ -1,10 +1,44 @@
-const About = () => {
-    return (
-        <div>
-            <h1>About</h1>
-            <h2>This is namaste react</h2>
-        </div>
-    )
+import User from "./User";
+import UserClass from "./UserClass";
+import React from "react";
+
+class About extends React.Component{
+    constructor(props)
+    {
+        super(props);
+        // console.log("Parent Constructor");
+    }
+
+
+   componentDidMount()
+    {
+        // console.log("Parent ComponentDidMount");
+        
+    }
+
+
+    render(){
+        // console.log("Parent Render")
+        return (
+            <div>
+                <h1>About from Class Component</h1>
+                <h2>This is namaste react</h2>
+                {/* <User name = "Yash Paota (function)" location = "Paota" /> */}
+                <UserClass  />
+            </div>
+        )
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
 
 export default About;
