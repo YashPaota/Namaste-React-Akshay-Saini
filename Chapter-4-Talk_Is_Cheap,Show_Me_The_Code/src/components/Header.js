ed00f7c4c0036ca65528e7052816b24473b8b94a
Yash Paota <yashpaota@gmail.com>
2023-10-23 16:27:37 +0530

    Added Grocery li in Header

--- Chapter-4-Talk_Is_Cheap,Show_Me_The_Code/src/components/Header.js
+++ Chapter-4-Talk_Is_Cheap,Show_Me_The_Code/src/components/Header.js
@@ -36,6 +36,9 @@ const Header = () => {
                     <li>
                          <Link to="/contact">Contact Us </Link>
                     </li>
+                    <li>
+                         <Link to="/grocery">Grocery </Link>
+                    </li>
                     <li>Cart</li>
                     <button className="login" onClick={() => 
                        {
