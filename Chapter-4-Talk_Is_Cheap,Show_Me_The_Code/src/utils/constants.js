049d284df19fd54f0db84ce15f67770342f57591
Yash Paota <yashpaota@gmail.com>
2023-10-25 16:02:33 +0530

    Formatted constants file

--- Chapter-4-Talk_Is_Cheap,Show_Me_The_Code/src/utils/constants.js
+++ Chapter-4-Talk_Is_Cheap,Show_Me_The_Code/src/utils/constants.js
@@ -1,8 +1,5 @@
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-
-// export const LOGO_URL = "https://img.freepik.com/free-vector/food-shopping-logo-template-design_460848-10299.jpg?w=740&t=st=1696847047~exp=1696847647~hmac=e7831d60ca773da28555b80df90902616102882797c4b412d839daa8ef545535";
-
 export const LOGO_URL = "https://cdn.dribbble.com/userupload/9903003/file/original-8eae5aec60527b67b7678e42aa2d8645.jpg?resize=1024x768";
 
 export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
