232f72392d4e121a4ded2c71c50288e3d024eb31
Yash Paota <yashpaota@gmail.com>
2023-10-27 16:49:41 +0530

    Added the cart number feature in header

--- Chapter-4-Talk_Is_Cheap,Show_Me_The_Code/src/components/Header.js
+++ Chapter-4-Talk_Is_Cheap,Show_Me_The_Code/src/components/Header.js
@@ -4,50 +4,50 @@ import { Link } from "react-router-dom";
 import QuickFoodNew from "../utils/QuickFoodNew.jpg";
 import { useContext } from "react";
 import UserContext from "../utils/UserContext";
+import { useSelector } from "react-redux";
 // import useOnlineStatus from "../utils/useOnlineStatus";
 
-
 const Header = () => {
-    
-    // const[btnNameReact , setBtnNameReact] = useState("Login");
-    // console.log("Header rendered")
+  // const[btnNameReact , setBtnNameReact] = useState("Login");
+  // console.log("Header rendered")
 
-    // const onlineStatus = useOnlineStatus();
+  // const onlineStatus = useOnlineStatus();
 
-    // useEffect(() => {
-    //     console.log(" useEffectCalled");
-    // },[btnNameReact]);
+  // useEffect(() => {
+  //     console.log(" useEffectCalled");
+  // },[btnNameReact]);
 
-    const {loggedIn} = useContext(UserContext);
-    
+  const { loggedIn } = useContext(UserContext);
+  const cartItems = useSelector((store) => store.cart.items);
 
-    
-    return(
-        <div className=" h-24 flex justify-between shadow-xl mb-2 sm:bg-yellow-50 lg:bg-white">
-            <div className="w-44 ml-12 ">
-              <Link to="/">
-                <img src = {QuickFoodNew} className="h-24"/>
-              </Link>
-            </div>
-            <div className="flex items-center">
-                <ul className="flex p-4 m-4">
-                    {/* <li className="px-4 hover:text-orange-700 transition-all duration-300 ease-in-out text-lg font-bold ">
+  return (
+    <div className=" h-24 flex justify-between shadow-xl mb-2 sm:bg-yellow-50 lg:bg-white">
+      <div className="w-44 ml-12 ">
+        <Link to="/">
+          <img src={QuickFoodNew} className="h-24" />
+        </Link>
+      </div>
+      <div className="flex items-center">
+        <ul className="flex p-4 m-4">
+          {/* <li className="px-4 hover:text-orange-700 transition-all duration-300 ease-in-out text-lg font-bold ">
                        Online Status : {onlineStatus ?  "🟢" : "🔴"}
                     </li> */}
-                    <li className="px-4  hover:text-orange-700 transition-all duration-300 ease-in-out text-lg font-bold">
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li className="px-4  hover:text-orange-700 transition-all duration-300 ease-in-out text-lg font-bold ">
-                         <Link to="/about">About</Link>
-                    </li>
-                    <li className="px-4  hover:text-orange-700 transition-all duration-300 ease-in-out text-lg font-bold ">
-                         <Link to="/contact">Contact Us </Link>
-                    </li>
-                    <li className="px-4  hover:text-orange-700 transition-all duration-300 ease-in-out text-lg font-bold">
-                         <Link to="/grocery">Grocery </Link>
-                    </li>
-                    <li className="px-4  hover:text-orange-700 transition-all duration-300 ease-in-out text-lg font-bold ">Cart</li>
-                    {/* <button className="login" onClick={() => 
+          <li className="px-4  hover:text-orange-700 transition-all duration-300 ease-in-out text-lg font-bold">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="px-4  hover:text-orange-700 transition-all duration-300 ease-in-out text-lg font-bold ">
+            <Link to="/about">About</Link>
+          </li>
+          <li className="px-4  hover:text-orange-700 transition-all duration-300 ease-in-out text-lg font-bold ">
+            <Link to="/contact">Contact Us </Link>
+          </li>
+          <li className="px-4  hover:text-orange-700 transition-all duration-300 ease-in-out text-lg font-bold">
+            <Link to="/grocery">Grocery </Link>
+          </li>
+          <li className="px-4  hover:text-orange-700 hover:cursor-pointer transition-all duration-300 ease-in-out text-lg font-bold ">
+            <Link to="/cart"> Cart - ({cartItems.length} items) </Link>
+          </li>
+          {/* <button className="login" onClick={() => 
                        {
                           btnNameReact === "Login" 
                           ? setBtnNameReact("Logout")
@@ -56,13 +56,13 @@ const Header = () => {
                     {btnNameReact}
                     </button> */}
 
-                    <li className="px-4  hover:text-orange-700 transition-all duration-300 ease-in-out text-lg font-bold ">{loggedIn}</li>
-
-                </ul>
-            </div>
-        </div>
-    )
-}
-
+          <li className="px-4  hover:text-orange-700 hover:cursor-pointer  transition-all duration-300 ease-in-out text-lg font-bold ">
+            {loggedIn}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
